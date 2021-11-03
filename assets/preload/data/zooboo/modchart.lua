fe325f4e17466c895198e10f63b9b4d1df77d3c9
Peeeaaayus <91822564+Peeeaaayus@users.noreply.github.com>
2021-11-03 19:52:52 +0200

    ihe

---
+++ assets/preload/data/zooboo/modchart.lua
@@ -0,0 +1,17 @@
+function start(song) -- do nothing
+
+end
+
+function update(elapsed)
+    if curStep == 352 then
+            showOnlyStrums = true
+    end
+end
+
+function beatHit(beat) -- do nothing
+
+end
+
+function stepHit(step) -- do nothing
+
+end
